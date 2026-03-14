8e77ded4818e4420ade56292bffe5337ddc97899
leoolivieri <olivierileo30@gmail.com>
2026-03-14 11:21:54 -0300

    learn(ap004-react-props): finalização

--- ap004-react-props/src/App.jsx
+++ ap004-react-props/src/App.jsx
@@ -1,31 +1,69 @@
-import Pedido from "./Pedido"
+import Pedido from './Pedido.jsx'
+import Cartao from './Cartao.jsx'
+import Feedback from './Feedback.jsx'
 
 export default () => {
-  return(
+  const textoOK = "Já recebi"
+  const textoNOK = "Ainda não recebi"
+  const funcaoOK = () => alert("Agradecemos o Feedback")
+  const funcaoNOK = () => alert("Verificaremos")
+  const componenteFeedback = (
+    <Feedback
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+      textoOK={textoOK}
+      textoNOK={textoNOK} />
+  )
+
+  return (
     <div className="container border">
       <div className="row">
-        <div className="col-12">      
+        <div className="col-12">
           {/* i.fa-solid.fa-hippo (emmet) */}
-          <i className="fa-solid fa-hippo fa-3x"></i>
-          </div>
+          <i className="fa-solid fa-hippo fa-4x"></i>
+        </div>
       </div>
       <div className="row">
         <div className="col sm-12 col-md-6 col-xl-3">
-            {/* .card>(.card-header+.card-body) */}
-            <Pedido /> 
+          <Cartao cabecalho="14/06/2026">
+            <Pedido
+              icon="camera"
+              title="Câmera"
+              description="Uma câmera 4k"
+            />
+            {componenteFeedback}
+          </Cartao>
         </div>
-                <div className="col sm-12 col-md-6 col-xl-3">
-               <Pedido 
-                data="22/02/2026"
-                icone="camera"
-                titulo="Câmera"
-                descricao="Uma câmera 4k"/> 
-                </div>
-                <div className="col sm-12 col-md-6 col-xl-3">
-                <Pedido />
+        <div className="col sm-12 col-md-6 col-xl-3">
+          <Cartao cabecalho="15/06/2026">
+            <Pedido
+              icon="book"
+              title="Livro"
+              description="Um bom livro"
+            />
+            {componenteFeedback}
+          </Cartao>
         </div>
-                <div className="col sm-12 col-md-6 col-xl-3">
-                <Pedido />
+        <div className="col sm-12 col-md-6 col-xl-3">
+          <Cartao cabecalho="16/06/2026">
+            <Pedido
+              icon="bicycle"
+              title="Bicicleta"
+              description="Uma bibicleta excepcional de 25 mil"
+            />
+            {componenteFeedback}
+          </Cartao>
+        </div>
+        <div className="col sm-12 col-md-6 col-xl-3">
+          <Cartao cabecalho="17/06/2026">
+            <Pedido
+              icon="pencil"
+              title="Lápis"
+              description="Um bom lápis"
+            />
+            {componenteFeedback}
+          </Cartao>
+
         </div>
       </div>
     </div>
